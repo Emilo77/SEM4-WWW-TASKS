5b0ed9c4cb30efa8a714b2651226755a3519f5a1
Kamil Bugała <kamilbugala9@gmail.com>
2022-06-03 00:56:57 +0200

    Add lab07 final

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-const wycieczki = require('./data.js')
+let wycieczki = require('./data.js')
 const express = require('express')
 const path = require('path');
 const app = express()
 const port = 8081
 
+
 /* Load views engine*/
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.set('static', path.join(__dirname, 'static'));
+app.use(express.static(path.join(__dirname, 'static')));
 
 /* Include 'public' directory */
 app.use(express.static('views'))
@@ -14,11 +18,8 @@ app.use('/public', express.static('public'))
 
 /* Render Homepage */
 app.get('/', (req, res) => {
-    res.render('home', wycieczki)
-})
-
-app.get('/wycieczka/', (req, res) => {
-    res.render('wycieczka')
+    console.log("weszło")
+    res.render('home', {wycieczki:wycieczki})
 })
 
 app.get('/formularz/', (req, res) => {
@@ -29,14 +30,35 @@ app.get('/strona-testowa/', (req, res) => {
     res.send('Strona testowa!\n')
 })
 
-app.get('/wycieczka/:number/', (req, res) => {
-    res.send('Wycieczka nr ' + req.params['number'])
+app.get('/wycieczka/', (req, res) => {
+    res.render('404');
 })
 
-app.get('/wycieczka/:number/tydzien/:week_number', (req, res) => {
-    let numer = req.params['number'];
-    let week_numer = req.params['week_number'];
-    res.send('Wycieczka nr ' + numer + ' w tygodniu ' + week_numer)
+app.get('/wycieczka/:n/', (req, res) => {
+    let number = parseInt(req.params['n']);
+    if (number >= 0 && number < wycieczki.array.length) {
+        let wycieczka = wycieczki.array[number];
+        res.render('wycieczka', {
+            wycieczka: wycieczka
+        })
+    } else {
+        res.render('404')
+    }
+})
+
+app.get('/wycieczka/:n/tydzien/:w', (req, res) => {
+    let number = parseInt(req.params['n']);
+    let week = parseInt(req.params['w']);
+
+    if (number >= 0 && number < wycieczki.array.length && week > 0 && week < 8) {
+        let wycieczka = wycieczki.array[number];
+        res.render('wycieczka', {
+            wycieczka: wycieczka,
+            tydzien: week
+        })
+    } else {
+        res.render('404')
+    }
 })
 
 app.get('/wycieczka-:q?nr=:n?&tydzien=:t?', (req, res) => {
@@ -55,18 +77,12 @@ app.get('/wycieczka-:q?nr=:n?&tydzien=:t?', (req, res) => {
 
     } else {
         let num = parseInt(number);
-
-        res.render('wycieczka', {
-            num,
-            wycieczki
-        })
+        res.render('wycieczka', wycieczki)
 
     }
-
-    // let week_numer = req.params['week_number'];
-
 })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
--- data.js
+++ data.js
@@ -1,16 +1,83 @@
-let wycieczki = [
+let array = [
+    {
+        id: 0,
+        name: 'gory',
+        image: 'gory.jpg',
+        title: 'Wysokie Góry',
+        title_2: 'Niezwykle strome',
+        body: 'Krótka wycieczka na ten właśnie szczyt. Niezwykle  ciekawe atrakcje.',
+        price: '15002900',
+
+        long_descripion: 'Po kawce w Gościńcu Równica mieliśmy znów sporo energii aby pokonywać kolejne kilometry.' +
+            ' Szlak dalej był już dość łagodny, z delikatnymi zejściami i podejściami. ' +
+            'Taka wędrówka w spokojnym tempie sprzyjała towarzyskim rozmowom ze znajomymi, ' +
+            'których zabraliśmy na tą wycieczkę. Jeśli nie lubicie zalesionych szczytów – spokojnie. ' +
+            'To przejście idealnie łączy ze sobą komfort, jaki daje las w upalny, letni dzień, ' +
+            'oraz możliwość cieszenia się widokami. Dość regularnie na całej trasie pojawiają się prześwity, ' +
+            'z któych roztaczały się przepiękne panoramy na zielony Beskid Śląski. ' +
+            'W trakcie wejścia na Kazbek, już od pierwszego dnia towarzyszyło nam kilku przewodników, ' +
+            'którzy dobierali tempo. Zdziwiło mnie, że jest ono bardzo spokojne. To jednak dobrze. ' +
+            'Po co tracić siły na szybki marsz, skoro plecaki są ciężkie, a czasu mamy sporo. ' +
+            'Po drodze mogliśmy spokojnie podziwiać gruzińskie góry, ' +
+            'które wciąż wydają się dzikie i niedostępne. Pełen turystów jest tutaj tylko jeden szlak. ' +
+            'Mowa oczywiście o tym prowadzącym na Kazbek. Po około 4 godzinach spokojnego marszu naszym oczom ' +
+            'ukazał się lodowiec Gergeti oraz nowo wybudowane schronisko Alti Hut. ' +
+            'Niestety ceny noclegu w schronisku są bardzo wysokie. Znacznie tańsze jest spanie w namiotach, ' +
+            'które do obozu dostarczyły nam konie. Naszym zadaniem było tylko rozbicie namiotu ' +
+            'i przygotowanie wody oraz posiłku.'
+    },
+
+
     {
         id: 1,
-        title: 'Wysokie Góry',
-        body: 'Krótka wycieczka na ten właśnie szczyt. Niezwykle  ciekawe atrakcje.',
-        price: '15002900'
+        name: 'morza',
+        image: 'morza.jpg',
+        title: 'Dalekie morza',
+        title_2: 'Niezwykle dalekie',
+        body: 'Mórz jest wiele, więc i opis może być nieco dłuższy niż poprzednio. Atrakcji może być więcej.',
+        price: '17',
+
+        long_descripion:
+            'Basowy huk fal, stada rozkrzyczanych mew szybujących nad wypływającym w morze statkiem, ' +
+            'tętniące życiem porty … Puste plaże bezlitośnie smagane ostrym, północno-zachodnim wiatrem, ' +
+            'ukryte w pachnąco-sosnowych lasach latarnie morskie, niepowtarzalność ' +
+            'i piękno każdego morskiego dnia… Unikalny klimat, kameralna atmosfera nadmorskich, ' +
+            'niewielkich miejscowości uśpionych morską kołysanką, zdanych na łaskę kapryśnego morza, ' +
+            'w których rytm życia wyznaczają wypływające w morze kutry i jesienno-zimowe sztormy… ' +
+            'Postaram się na łamach witryny przedstawić choć część tego, ' +
+            'co od urodzenia jest mi bardzo bliskie. ' +
+            'Być może uda mi się przy pomocy tego serwisu przekonać Was, ' +
+            'że warto jest mieszkać blisko Wielkiej Wody, że nadmorskie miejscowości i porty emanują ' +
+            'niespotykaną atmosferą, która uzależnia jak narkotyk kolejne pokolenia. ' +
+            'Chciałbym też przybliżyć Wam szeroko pojętą Sprawę Morską, życie na morzu, ' +
+            'kulisy ciężkiego zawodu marynarza. Tradycję i Kulturę Morza tworzyli od podstaw wielcy ludzie, ' +
+            'kontynuują ją każdego dnia kolejne pokolenia Żeglarzy, Marynarzy, Oficerów, Kapitanów. ' +
+            'Okazujmy im zasłużony szacunek.'
     },
+
     {
         id: 2,
-        title: 'Dalekie morza',
-        body: 'Mórz jest wiele, więc i opis może być nieco dłuższy niż poprzednio. Atrakcji może być więcej.',
-        price: '17'
-    },
-]
+        name: 'mediolan',
+        image: 'mediolan.jpg',
+        title: 'Mediolan',
+        title_2: 'JESTEŚMY W MEDIOLANIEEE',
+        body: 'Ciuszki i super exclusive rzeczy w przystępnych cenach. Raj dla fanów mody',
+        price: '2193123',
 
-module.exports = {wycieczki};
+        long_descripion:
+            'Weekend w Mediolanie? W czasie pandemii? To mocno przewrotne i zdecydowanie w naszym stylu. ' +
+            'Gdy rzucamy hasło by gdzieś polecieć po pandemii to najczęściej słyszymy, ' +
+            'że Włochy są opcją ostatniego wyboru. A naszym zdaniem to teraz idealny i bezpieczny ' +
+            'kierunek bo właśnie Włosi, którzy zostali tak ciężko doświadczeni przez los i pandemię, ' +
+            'wzbili się na wyżyny dbałości o przestrzeganie reżimu sanitarnego czego doświadczyliśmy ' +
+            'na własnej skórze i byliśmy pozytywnie zaskoczeni.' +
+            'Lot samolotem odbywa się w klasycznym reżimie, gdzie obowiązuje ograniczenie połowy zajętości miejsc. ' +
+            'Słowem w samolocie co drugie miejsce jest wolne. Obowiązkowo w trakcie całego lotu należy mieć ' +
+            'założoną maseczkę. Obsługa przestrzega by nie tworzyć kolejek do toalet i czekać na swoją kolej na miejscu. ' +
+            'Biegiem można zobaczyć dużo ale też ominąć smaczki. Dlatego wychodzimy z założenia ' +
+            'lepiej mniej a dobrze. Chociaż nam udało się w trakcie weekendu zobaczyć całkiem sporo.'
+    }
+];
+
+
+module.exports = {array};
